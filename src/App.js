cbee10206c2b69b187ff58b3540ed912667a3af5
marcinzygan <soul@o2.pl>
2022-03-29 22:24:53 +0100

    added hold dice function , re-roll  dice that is not held , check for win conditions , check for num of turns passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { nanoid } from 'nanoid'
 import Dice from "./Dice"
+import Confetti from 'react-confetti'
 
 export default function App(){
 //Set state for allNewDice
 
 const [dice , setDice] = React.useState(allNewDice())
 
+// Set state for winCheck 
 
+const [winCheck , setWinCheck] = React.useState(false)
+
+
+//Use effect to keep two states in sync , and everytime dice array is changed 
+React.useEffect(()=> {
+const firstValue = dice[0].value;
+const allTheSame = dice.every(die => firstValue === die.value)
+const allHeld = dice.every(die=> die.isHeld)
+ if(allHeld && allTheSame){
+   setWinCheck(true)
+ }
+
+},[dice])
+
+
+//Function to generate new dice object 
+function generateNewDice(){
+  return{
+     value: Math.floor(Math.random() *6 +1), 
+     isHeld: false ,
+     id:nanoid()
+  }
+}
 //Generate array with 10 random numbers between 1-6 each number is an oobject 
 function allNewDice(){
  let newDice = []
  for( let i=0 ; i<10 ; i++){
-   newDice.push({ 
-     value: Math.floor(Math.random() *6 +1), 
-     isHeld: false ,
-     id:nanoid()
-    })
+   newDice.push( 
+     generateNewDice()
+    )
  }
  return newDice
 }
 
+
+//Funcion holdDice 
+
+function holdDice(id){
+setDice(oldDice => oldDice.map(dice =>{
+ return dice.id === id ? 
+ {...dice , isHeld: !dice.isHeld} :
+ dice
+}))
+
+}
+const [turn , setTurn] = React.useState(1)
+
+function checkTurn(){
+  setTurn(prevTurn => prevTurn +1)
+}
+
+// Roll new Dice , if there is win roll all new 
+function diceRoll(){
+  if(!winCheck){setDice(oldDice => oldDice.map(dice => {
+    return dice.isHeld  ?
+     dice : 
+    generateNewDice()
+  }
+  ))}else{
+    setWinCheck(false);
+    setTurn(1);
+    setDice(allNewDice())
+  }
+  
+  
+  
+}
+  
 
 // Map over dice array and for each item create new Dice component with item value .
 const dices = dice.map(item =>
@@ -28,19 +85,23 @@ const dices = dice.map(item =>
    key={item.id}
    value={item.value}
    isHeld={item.isHeld}
+   holdDice ={()=> holdDice(item.id)}
+
    />)
-//Roll new Dices and set it to state
 
-function diceRoll(){
-  setDice(allNewDice())
-}
-
+  
   return(
     <main>
+      {winCheck && 
+      <Confetti
+      className="confetti"
+      />}
+      <h1 className="title">{winCheck? "You Won " : "Tenzies"}</h1>
+      <p className="instructions">{winCheck? `Number of Turns: ${turn}`  : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}</p>
       <div className="dice__container">
         {dices}
       </div>
-      <button className="btn" onClick={diceRoll}>Roll</button>
+      <button className="btn" onClick={()=>{checkTurn(); diceRoll();}}>{winCheck ? "New Game": "Roll"}</button>
     </main>
   )
 }
--- src/Dice.js
+++ src/Dice.js
@@ -1,7 +1,7 @@
 export default function Dice(props){
    const  styles={backgroundColor: props.isHeld ? "#59E391" : "white"}
     return (
-        <div className="dice" style={styles} >
+        <div className="dice" onClick={props.holdDice} style={styles} >
         <p className="dice__value" >{props.value} </p>
         </div>
     )
