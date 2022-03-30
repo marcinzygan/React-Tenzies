ae24dd683b6fb51cbdbe1fcfc779a06d2a501cdb
marcinzygan <soul@o2.pl>
2022-03-30 19:38:15 +0100

    added local storage function to keep track of fastesTime and fastestTurns

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React,  {useEffect}  from "react"
 import { nanoid } from 'nanoid'
 import Dice from "./Dice"
 import Confetti from 'react-confetti'
@@ -14,12 +14,14 @@ const [winCheck , setWinCheck] = React.useState(false)
 
 
 //Use effect to keep two states in sync , and everytime dice array is changed 
-React.useEffect(()=> {
+useEffect(()=> {
 const firstValue = dice[0].value;
 const allTheSame = dice.every(die => firstValue === die.value)
 const allHeld = dice.every(die=> die.isHeld)
  if(allHeld && allTheSame){
    setWinCheck(true)
+   setIsTimeActive(false)
+   
  }
 
 },[dice])
@@ -46,7 +48,6 @@ function allNewDice(){
 
 
 //Funcion holdDice 
-
 function holdDice(id){
 setDice(oldDice => oldDice.map(dice =>{
  return dice.id === id ? 
@@ -55,11 +56,44 @@ setDice(oldDice => oldDice.map(dice =>{
 }))
 
 }
-const [turn , setTurn] = React.useState(1)
+
+
+//Check for number of turns
+const [turns , setTurns] = React.useState(1)
 
 function checkTurn(){
-  setTurn(prevTurn => prevTurn +1)
+  setTurns(prevTurn => prevTurn +1)
 }
+
+//Time tracker to check how fast you finished the game
+const [time ,setTime] = React.useState(0)
+const [isTimeActive , setIsTimeActive] = React.useState(true)
+
+useEffect(() => {
+
+  let interval = null;
+  if(isTimeActive){
+  interval = setInterval(() => {
+      setTime(time => time + 1);
+    }, 1000);
+}else if (!isTimeActive && time !== 0){
+  clearInterval(interval)
+}
+  return () => clearInterval(interval);
+}, [isTimeActive ,time]);
+
+//Get Local storage 
+const [BestTime , setBestTime] = React.useState(JSON.parse(localStorage.getItem("BestTime")) || 9999)
+const [BestTurn , setBestTurn] = React.useState(JSON.parse(localStorage.getItem("BestTurn")) || 9999)
+//Set Local Storage
+useEffect(() => {
+  localStorage.setItem("BestTime", JSON.stringify(BestTime))
+}, [BestTime])
+
+useEffect(() => {
+  localStorage.setItem("BestTurn", JSON.stringify(BestTurn))
+}, [BestTurn])
+console.log(BestTime)
 
 // Roll new Dice , if there is win roll all new 
 function diceRoll(){
@@ -70,8 +104,12 @@ function diceRoll(){
   }
   ))}else{
     setWinCheck(false);
-    setTurn(1);
+    setTurns(1);
+    setIsTimeActive(true)
+    setTime(0)
     setDice(allNewDice())
+    setBestTime(prevTime =>  prevTime > time ? time : prevTime)
+    setBestTurn(prevTurn =>  prevTurn > turns ? turns : prevTurn)
   }
   
   
@@ -91,17 +129,22 @@ const dices = dice.map(item =>
 
   
   return(
+    <div>
+      <p className="stats">Fastest Win:<span className="score"> {BestTurn} turns.</span></p>
+      <p className="stats">Fastest Time:<span className="score"> {BestTime} seconds.</span></p>
     <main>
       {winCheck && 
       <Confetti
       className="confetti"
       />}
       <h1 className="title">{winCheck? "You Won " : "Tenzies"}</h1>
-      <p className="instructions">{winCheck? `Number of Turns: ${turn}`  : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}</p>
+      <p className="instructions">{winCheck? `Number of Turns: ${turns} `  : "Roll until all dice are the same. Click each die to freeze it at its current value between rolls."}</p>
+      {winCheck && <p className="time">{`Time : ${time} seconds`}</p>}
       <div className="dice__container">
         {dices}
       </div>
       <button className="btn" onClick={()=>{checkTurn(); diceRoll();}}>{winCheck ? "New Game": "Roll"}</button>
     </main>
+    </div>
   )
 }
